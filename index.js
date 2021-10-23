ebe96b05aab370bc7314bea38ce3e4529bc3bdca
Maciej Monkiewicz <70110809+juanmeister420@users.noreply.github.com>
2021-10-23 13:02:54 +0200

    🧠 1.0.0@main
    Created function, which is getting data from MZ server

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const axios = require('axios')
-const cheerio = require('cheerio')
 const app = express()
 const PORT = 3000
+
+function get_data(){
+    axios.get('https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_corona_actual_widok3/FeatureServer/0/query?f=json&where=1%3D1&outFields=*')
+    .then(response => {
+    if(response.status === 200) {
+        var obj = response.data.features[0].attributes
+        delete obj.OBJECTID
+        return data = obj
+    }else{
+        return data = {
+            error: true,
+            status: response.status
+        }
+    }
+    })
+}
 
 app.get('/help', (req, res) => {
     res.json({
